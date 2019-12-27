8842325a89de4b24149897c1b0956fdd2e7637ef
HeliumDog <admin@aiblockly.com>
2019-12-27 17:19:38 +0800

    Create depandency.sh

---
+++ depandency.sh
@@ -0,0 +1,9 @@
+sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install -y --no-install-recommends libboost-all-dev
+sudo apt-get install -y libopenblas-dev liblapack-dev libatlas-base-dev
+sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev
+sudo apt-get install -y cmake build-essential
+#caffe also can work well without opencv
+##↓↓↓↓↓↓↓if you want compile with opencv pls uncommet ↓↓↓↓↓↓↓↓↓↓
+#apt install libopencv-dev
+
